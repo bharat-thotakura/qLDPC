31867e0021a20d567b012dae90b4b6b738dbd962
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-08-30 23:19:37 -0400

    clean up syndrome_measurement_test.py

--- qldpc/circuits/syndrome_measurement_test.py
+++ qldpc/circuits/syndrome_measurement_test.py
@@ -28,43 +28,51 @@ from qldpc.objects import Pauli
 
 def test_syndrome_measurement(pytestconfig: pytest.Config) -> None:
     """Verify that syndromes are read out correctly."""
-    random.seed(pytestconfig.getoption("randomly_seed"))
+    seed = pytestconfig.getoption("randomly_seed")
 
-    # classical seed codes for a random HGPCode
-    code_a = codes.ClassicalCode.random(5, 3, seed=random.randint(0, 2**32))
-    code_b = codes.ClassicalCode.random(3, 2, seed=random.randint(0, 2**32))
+    # default strategies for non-CSS and CSS codes
+    assert_valid_syndome_measurement(codes.FiveQubitCode())
+    assert_valid_syndome_measurement(codes.SteaneCode())
 
-    for code, strategy in [
-        (codes.FiveQubitCode(), circuits.EdgeColoring()),
-        (codes.SteaneCode(), circuits.EdgeColoring()),
-        (codes.SteaneCode(), circuits.EdgeColoringXZ()),
-        (codes.HGPCode(code_a, code_b), circuits.EdgeColoring()),
-        (codes.ToricCode(2, rotated=True), circuits.EdgeColoring()),
-        (codes.SurfaceCode(2, rotated=True), circuits.EdgeColoring()),
-    ]:
-        # prepare a logical |0> state
-        state_prep = circuits.get_encoding_circuit(code)
+    # special strategies for toric and surface codes
+    assert_valid_syndome_measurement(codes.ToricCode(2, rotated=True, field=2))
+    assert_valid_syndome_measurement(codes.SurfaceCode(2, rotated=True, field=2))
 
-        # apply random Pauli errors to the data qubits
-        errors = random.choices([Pauli.I, Pauli.X, Pauli.Y, Pauli.Z], k=len(code))
-        error_ops = stim.Circuit()
-        for qubit, pauli in enumerate(errors):
-            if pauli is not Pauli.I:  # I_ERROR is only recognized in stim>=1.15.0
-                error_ops.append(f"{pauli}_error", [qubit], [1])
+    # special strategy for HGPCodes
+    code_a = codes.ClassicalCode.random(5, 3, seed=seed)
+    code_b = codes.ClassicalCode.random(3, 2, seed=seed + 1)
+    assert_valid_syndome_measurement(codes.HGPCode(code_a, code_b, field=2))
 
-        # measure syndromes
-        syndrome_extraction, record = strategy.get_circuit(code)
-        for check in range(len(code), len(code) + code.num_checks):
-            syndrome_extraction.append("DETECTOR", record.get_target_rec(check))
-
-        # sample the circuit to obtain a syndrome vector
-        circuit = state_prep + error_ops + syndrome_extraction
-        syndrome = circuit.compile_detector_sampler().sample(1).ravel()
-
-        # compare against the expected syndrome
-        error_xz = code.field([pauli.value for pauli in errors]).T.ravel()
-        expected_syndrome = code.matrix @ symplectic_conjugate(error_xz)
-        assert np.array_equal(expected_syndrome, syndrome)
-
+    # EdgeColoringXZ strategy
+    assert_valid_syndome_measurement(codes.SteaneCode(), circuits.EdgeColoringXZ())
     with pytest.raises(ValueError, match="only supports CSS codes"):
         circuits.EdgeColoringXZ().get_circuit(codes.FiveQubitCode())
+
+
+def assert_valid_syndome_measurement(
+    code: codes.QuditCode, strategy: circuits.SyndromeMeasurementStrategy = circuits.EdgeColoring()
+) -> None:
+    """Assert that the syndrome measurement of the given code with the given strategy is valid."""
+    # prepare a logical |0> state
+    state_prep = circuits.get_encoding_circuit(code)
+
+    # apply random Pauli errors to the data qubits
+    errors = random.choices([Pauli.I, Pauli.X, Pauli.Y, Pauli.Z], k=len(code))
+    error_ops = stim.Circuit()
+    for qubit, pauli in enumerate(errors):
+        if pauli is not Pauli.I:  # I_ERROR is only recognized in stim>=1.15.0
+            error_ops.append(f"{pauli}_error", [qubit], [1])
+
+    # measure syndromes
+    syndrome_extraction, record = strategy.get_circuit(code)
+    for check in range(len(code), len(code) + code.num_checks):
+        syndrome_extraction.append("DETECTOR", record.get_target_rec(check))
+
+    # sample the circuit to obtain a syndrome vector
+    circuit = state_prep + error_ops + syndrome_extraction
+    syndrome = circuit.compile_detector_sampler().sample(1).ravel()
+
+    # compare against the expected syndrome
+    error_xz = code.field([pauli.value for pauli in errors]).T.ravel()
+    expected_syndrome = code.matrix @ symplectic_conjugate(error_xz)
+    assert np.array_equal(expected_syndrome, syndrome)
